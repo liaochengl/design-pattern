0db32fe089dbd85b6088d3faf2d0d036572e5b5a
lanyang <liaochengli@163.com>
2019-12-16 09:38:47 +0800

    中介者模式

---
+++ src/io/lanyang/mediator/smarthouse/Curtains.java
@@ -0,0 +1,19 @@
+package io.lanyang.mediator.smarthouse;
+
+public class Curtains extends Colleague {
+
+	public Curtains(Mediator mediator, String name) {
+		super(mediator, name);
+		mediator.register(name, this);
+	}
+
+	@Override
+	public void sendMessage(int stateChange) {
+		this.getMediator().getMessage(stateChange, this.name);
+	}
+
+	public void upCurtains() {
+		System.out.println("I am holding Up Curtains!");
+	}
+
+}
---
+++ src/io/lanyang/mediator/smarthouse/TV.java
@@ -0,0 +1,22 @@
+package io.lanyang.mediator.smarthouse;
+
+public class TV extends Colleague {
+
+	public TV(Mediator mediator, String name) {
+		super(mediator, name);
+		mediator.register(name, this);
+	}
+
+	@Override
+	public void sendMessage(int stateChange) {
+		this.getMediator().getMessage(stateChange, this.name);
+	}
+
+	public void startTv() {
+		System.out.println("It's time to StartTv!");
+	}
+
+	public void stopTv() {
+		System.out.println("StopTv!");
+	}
+}
